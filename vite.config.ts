b1ba56770a6d09c30abf0efc779b1dd98c62c25a
ageha734 <hibikeita18@gmail.com>
2025-01-10 16:43:24 +0900

    update: vercel -> cloudflare

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { vitePlugin as remix, cloudflareDevProxyVitePlugin } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [cloudflareDevProxyVitePlugin(), remix(), tsconfigPaths()],
+});
