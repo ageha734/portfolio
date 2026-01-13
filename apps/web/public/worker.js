e8a461d620e040a79b032f529d8149c794594fa2
ageha734 <hibi-keita@dmm.com>
2026-01-13 12:56:08 +0900

    chore: update package.json scripts for linting and add new GitHub Action configurations for improved CI/CD processes

--- apps/web/public/worker.js
+++ apps/web/public/worker.js
@@ -4,8 +4,8 @@ console.log("🏎️💨 Service worker registered");
  * @external https://sergiodxa.com/articles/using-service-workers-with-remix
  */
 self.addEventListener("fetch", (event) => {
-    let url = new URL(event.request.url);
-    let method = event.request.method;
+    const url = new URL(event.request.url);
+    const method = event.request.method;
 
     // any non GET request is ignored
     if (method.toLowerCase() !== "get") return;
@@ -22,12 +22,12 @@ self.addEventListener("fetch", (event) => {
                 .open("assets")
                 .then(async (cache) => {
                     // if the request is cached we will use the cache
-                    let cacheResponse = await cache.match(event.request);
+                    const cacheResponse = await cache.match(event.request);
                     if (cacheResponse) return cacheResponse;
 
                     // if it's not cached we will run the fetch, cache it and return it
                     // this way the next time this asset it's needed it will load from the cache
-                    let fetchResponse = await fetch(event.request);
+                    const fetchResponse = await fetch(event.request);
                     cache.put(event.request, fetchResponse.clone());
 
                     return fetchResponse;
