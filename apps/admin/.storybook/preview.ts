37d07af8e21590a861df77d323cfd85f3ecb9765
ageha734 <hibi-keita@dmm.com>
2026-01-18 06:35:24 +0900

    chore: remove unused Storybook configurations and assets across admin and wiki applications; update package.json files to remove deprecated dependencies

--- apps/admin/.storybook/preview.ts
+++
@@ -1,9 +0,0 @@
-import { createStorybookPreviewPreset } from "@portfolio/storybook-config/preview";
-import "@portfolio/ui/tailwind.css";
-import "~/tailwind.css";
-
-const preview = createStorybookPreviewPreset({
-    tailwindCssPath: "@portfolio/ui/tailwind.css",
-});
-
-export default preview;
