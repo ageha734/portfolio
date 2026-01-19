5292ca39e34c7cfde7ccc1aabf809daa267b6bb7
ageha734 <hibi-keita@dmm.com>
2026-01-20 03:15:58 +0900

    chore: update TypeScript definitions and generated files for portfolios and posts, ensuring consistent import syntax and improving code clarity

--- packages/db/dist/seed/user.d.ts
+++ packages/db/dist/seed/user.d.ts
@@ -1,10 +1,10 @@
 import type { PrismaClient } from "@prisma/client";
 export declare function seedUser(prisma: PrismaClient): Promise<{
-    id: string;
     email: string;
+    id: string;
+    image: string | null;
     name: string;
     emailVerified: boolean;
-    image: string | null;
     bio: string | null;
     createdAt: Date;
     updatedAt: Date;
