e445c38a8b60995c4d4563074444e00503d00569
ageha734 <hibi-keita@dmm.com>
2026-01-26 23:25:43 +0900

    refactor: standardize deployment scripts across applications to improve consistency and simplify configuration management

--- packages/ui/dist/libs/sanitize.js
+++ packages/ui/dist/libs/sanitize.js
@@ -1,42 +1,47 @@
-import DOMPurify from "isomorphic-dompurify";
+const SANITIZE_CONFIG = {
+    ALLOWED_TAGS: [
+        "p",
+        "br",
+        "strong",
+        "em",
+        "u",
+        "s",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "ul",
+        "ol",
+        "li",
+        "a",
+        "img",
+        "blockquote",
+        "code",
+        "pre",
+        "span",
+        "div",
+        "table",
+        "thead",
+        "tbody",
+        "tr",
+        "th",
+        "td",
+    ],
+    ALLOWED_ATTR: ["href", "src", "alt", "title", "class", "id", "target", "rel"],
+    ALLOW_DATA_ATTR: false,
+};
 /**
  * HTMLコンテンツをサニタイズする
+ * サーバーサイド（Cloudflare Workers）ではそのまま返し、クライアントサイドでのみサニタイズする
  * @param html - サニタイズするHTML文字列
  * @returns サニタイズされたHTML文字列
  */
 export const sanitizeHtml = (html) => {
-    return DOMPurify.sanitize(html, {
-        ALLOWED_TAGS: [
-            "p",
-            "br",
-            "strong",
-            "em",
-            "u",
-            "s",
-            "h1",
-            "h2",
-            "h3",
-            "h4",
-            "h5",
-            "h6",
-            "ul",
-            "ol",
-            "li",
-            "a",
-            "img",
-            "blockquote",
-            "code",
-            "pre",
-            "span",
-            "div",
-            "table",
-            "thead",
-            "tbody",
-            "tr",
-            "th",
-            "td",
-        ],
-        ALLOWED_ATTR: ["href", "src", "alt", "title", "class", "id", "target", "rel"],
-        ALLOW_DATA_ATTR: false,
-    });
+    if (typeof window === "undefined") {
+        return html;
+    }
+    const DOMPurify = require("isomorphic-dompurify").default;
+    return DOMPurify.sanitize(html, SANITIZE_CONFIG);
 };
